e9e53bd4580fe2c3416de21f9f5b365e0cd832bb
Dario Seyb <dario.seyb@gmail.com>
2023-05-05 10:49:05 -0400

    cluster changes to scenes and run script

--- run-tungsten.sh
+++ run-tungsten.sh
@@ -12,15 +12,15 @@
 # Number of CPUs per task
 #SBATCH --cpus-per-task=1
 
-#SBATCH -o ./report/output.%x.%j.%a.out # STDOUT
+#SBATCH -o ./report/output.%j.%a.out # STDOUT
 
 #SBATCH --array=1-128
 # Request memory
 #SBATCH --mem=1G
 # Walltime (job duration)
-#SBATCH --time=01:30:00
+#SBATCH --time=03:00:00
 # Email notifications (comma-separated options: BEGIN,END,FAIL)
 #SBATCH --mail-type=FAIL
 export JOB_DIR="/dartfs-hpc/rc/lab/J/JaroszLab/dseyb/stimp/${SLURM_JOB_NAME}/$((${SLURM_ARRAY_JOB_ID}))"
 mkdir -p $JOB_DIR
-./build/tungsten --threads 1 --spp 1 --seed $((${SLURM_ARRAY_TASK_ID})) -d $JOB_DIR -o "$((${SLURM_ARRAY_TASK_ID})).png" -e "$((${SLURM_ARRAY_TASK_ID})).pfm" "./data/example-scenes/gp-medium-cornell-box/${SLURM_JOB_NAME}.json" 
+./build/tungsten --threads 1 --spp 8 --seed $((${SLURM_ARRAY_TASK_ID})) -d $JOB_DIR -o "$((${SLURM_ARRAY_TASK_ID})).png" -e "$((${SLURM_ARRAY_TASK_ID})).pfm" "./data/example-scenes/${SLURM_JOB_NAME}.json" 
