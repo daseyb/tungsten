195c53a7016b291e76b905c552fc2003e6b432cd
Dario Seyb <dario.seyb@gmail.com>
2023-11-23 18:12:45 -0500

    cluster run settings

--- run-tungsten.sh
+++ run-tungsten.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks-per-node=1
 
 # Number of CPUs per task
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=10
 
 #SBATCH -o ./report/output-render.%A.%a.out # STDOUT
 
@@ -23,4 +23,4 @@
 #SBATCH --mail-type=FAIL
 export JOB_DIR="/dartfs-hpc/rc/lab/J/JaroszLab/dseyb/stimp/${SLURM_JOB_NAME}/$((${SLURM_ARRAY_JOB_ID}))"
 mkdir -p $JOB_DIR
-./build/tungsten --threads 1 --spp 1 --seed $((${SLURM_ARRAY_TASK_ID})) -d $JOB_DIR -o "$((${SLURM_ARRAY_TASK_ID})).png" -e "$((${SLURM_ARRAY_TASK_ID})).exr" "./data/example-scenes/${SLURM_JOB_NAME}.json" 
+./build/tungsten --threads 10 --spp 1 --seed $((${SLURM_ARRAY_TASK_ID})) -d $JOB_DIR -o "$((${SLURM_ARRAY_TASK_ID})).png" -e "$((${SLURM_ARRAY_TASK_ID})).exr" "./data/example-scenes/${SLURM_JOB_NAME}.json" 
